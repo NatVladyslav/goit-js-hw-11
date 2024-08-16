60489965ecee7aed8351291b78ee19b589b66c75
NatVladyslav <99360469+NatVladyslav@users.noreply.github.com>
2024-08-16 10:02:29 +0000

    Deploying to gh-pages from @ NatVladyslav/goit-js-hw-11@34e8542769b7172c505d4bef4be8497e9c0621dd 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,2 +1,24 @@
-(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))s(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const o of t.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&s(o)}).observe(document,{childList:!0,subtree:!0});function i(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function s(e){if(e.ep)return;e.ep=!0;const t=i(e);fetch(e.href,t)}})();
+(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const o of t.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&i(o)}).observe(document,{childList:!0,subtree:!0});function s(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function i(e){if(e.ep)return;e.ep=!0;const t=s(e);fetch(e.href,t)}})();function n(){return`
+    <li>
+        <img src="" alt="" />
+        <ul>
+          <li>
+            <h3>Likes</h3>
+            <p>info:</p>
+          </li>
+          <li>
+            <h3>Views</h3>
+            <p>info:</p>
+          </li>
+          <li>
+            <h3>Comments</h3>
+            <p>info:</p>
+          </li>
+          <li>
+            <h3>Downloads</h3>
+            <p>info:</p>
+          </li>
+        </ul>
+    </li>
+    `}console.log(n());
 //# sourceMappingURL=commonHelpers.js.map
